3e3065ef2f992abea0bc405b5df24a32b10174b9
dimden <admin@dimden.dev>
2023-05-11 19:39:54 +0300

    set url patterns

--- background.js
+++ background.js
@@ -1,7 +1,49 @@
+let patterns = [
+    "*://*.bilibili.com/video/*",
+
+    "*://*.instagram.com/p/*",
+    "*://*.instagram.com/reels/*",
+    "*://*.instagram.com/reel/*",
+
+    "*://*.twitter.com/*/status/*",
+    "*://*.twitter.com/*/status/*/video/*",
+    "*://*.twitter.com/i/spaces/*",
+    
+    "*://*.reddit.com/r/*/comments/*/*",
+
+    "*://*.soundcloud.com/*/*",
+    "*://*.soundcloud.app.goo.gl/*",
+
+    "*://*.tumblr.com/post/*",
+    "*://*.tumblr.com/*/*",
+    "*://*.tumblr.com/*/*/*",
+    "*://*.tumblr.com/blog/view/*/*",
+
+    "*://*.tiktok.com/*",
+    
+    "*://*.vimeo.com/*",
+
+    "*://*.youtube.com/watch?*",
+    "*://*.youtu.be/*",
+
+    "*://*.vk.com/video*",
+    "*://*.vk.com/*?w=wall*",
+    "*://*.vk.com/clip*",
+
+    "*://*.vine.co/*",
+];
+
 chrome.contextMenus.create({
     title: "Download media from link",
     contexts: ["link"],
-    id: "download-media-from-link"
+    id: "download-media-from-link",
+    targetUrlPatterns: patterns
+});
+chrome.contextMenus.create({
+    title: "Download media from this page",
+    contexts: ["page"],
+    id: "download-media-from-page",
+    documentUrlPatterns: patterns
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
@@ -14,5 +56,14 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
                 });
             }
         );
-    };
+    } else if(info.menuItemId === "download-media-from-page") {
+        chrome.storage.sync.get(
+            { instance: 'co.wukko.me' },
+            (items) => {
+                chrome.tabs.create({
+                    url: `https://${items.instance}/?u=${tab.url}`
+                });
+            }
+        );
+    }
 });
